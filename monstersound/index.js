4af6221c4ff326f5e7cfb797d2e7fe6ecc9427ca
javierbyte <hi@javierbyte.com>
2015-10-27 19:20:08 -0600

    Add monster sound demo script

---
+++ monstersound/index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var Player = require('player');
+
+var songs = [
+  path.join(__dirname, './mp3/monster.mp3')
+]
+
+new Player(songs)
+  .on('playing', function(song) {
+    console.log('I\'m playing... ');
+  })
+  .on('playend', function(song) {
+    console.log('Play done ...');
+  })
+  .on('error', function(err) {
+    console.log('Opps...!')
+    console.log(err);
+  })
+  .play()
